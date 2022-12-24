2a73903104e927b000655d55e95372f29393d2e6
Maksym Golik <maksym.golik@gmail.com>
2022-12-24 23:05:43 +0200

    started developing hibernate config

--- src/main/java/com/application/config/AppDispatcherServletInitializer.java
+++ src/main/java/com/application/config/AppDispatcherServletInitializer.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class AppDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{HibernateConfiguration.class};
     }
 
     @Override
