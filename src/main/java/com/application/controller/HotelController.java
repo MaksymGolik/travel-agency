49fd4a929dfbdfdf5bfdc84a93d8dcb0dfc114b1
lilia <liliamokhnatska@gmail.com>
2022-12-30 17:21:27 +0200

    class HotelController is created without logic

---
+++ src/main/java/com/application/controller/HotelController.java
@@ -0,0 +1,13 @@
+package com.application.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hotels")
+public class HotelController {
+
+
+
+}
