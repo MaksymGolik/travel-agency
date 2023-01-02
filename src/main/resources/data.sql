e7f0223ee36f146a576af7bb60a79d8767e2dfd1
lilia <liliamokhnatska@gmail.com>
2023-01-02 19:05:24 +0200

    create  data.sql is updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 
 INSERT INTO users (id,  first_name, last_name, email, password, phone_number, role) VALUES (1, 'Nick', 'Green', 'nick@mail.com', '$2a$04$/5OH4XrS8m2JOgRCIaFMwOoy4eFW6cgCNSRyLOvDD/6hLYHHDNJiG', +380559488336,'USER');
-INSERT INTO users (id,  first_name, last_name, email, password, phone_number, role) VALUES (2, 'Nora', 'White', 'nora@mail.com', '$2a$10$yYQaJrHzjOgD5wWCyelp0e1Yv1KEKeqUlYfLZQ1OQvyUrnEcX/rOy', +380589488336, USER);
-INSERT INTO users (id,  first_name, last_name, email, password, phone_number, role) VALUES (3, 'Mike', 'Brown', 'mike@mail.com', '$2a$04$fuyCLq/wrTKA2olt4Eugy.LxWvXsMc7mrd6F5YvCeGApcdp3XQeOC',+380599488336, USER);
+INSERT INTO users (id,  first_name, last_name, email, password, phone_number, role) VALUES (2, 'Nora', 'White', 'nora@mail.com', '$2a$10$yYQaJrHzjOgD5wWCyelp0e1Yv1KEKeqUlYfLZQ1OQvyUrnEcX/rOy', +380589488336, 'MANAGER');
+INSERT INTO users (id,  first_name, last_name, email, password, phone_number, role) VALUES (3, 'Mike', 'Brown', 'mike@mail.com', '$2a$04$fuyCLq/wrTKA2olt4Eugy.LxWvXsMc7mrd6F5YvCeGApcdp3XQeOC',+380599488336, 'USER');
 
 INSERT INTO rooms (id,  number_of_room, people_capacity, price, room_type, hotel_id) VALUES (1, 235, 2, 5000.0, 'STD', 1);
 INSERT INTO rooms (id,  number_of_room, people_capacity, price, room_type, hotel_id) VALUES (2, 132, 3, 8000.0, 'SUITE_MINI', 3);
