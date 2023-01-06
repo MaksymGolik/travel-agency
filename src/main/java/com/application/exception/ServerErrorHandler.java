48d4429cf065bfe652ea055c676cc9cae1adc2bb
Maksym Golik <maksym.golik@gmail.com>
2023-01-06 14:45:24 +0200

    Changes in exception handling

--- src/main/java/com/application/exception/ServerErrorHandler.java
+++ src/main/java/com/application/exception/ServerErrorHandler.java
@@ -7,10 +7,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
-
-
 
 @ControllerAdvice
 @Slf4j
@@ -18,12 +14,10 @@ public class ServerErrorHandler {
 
     @ExceptionHandler(Exception.class)
     public ModelAndView handler(Exception e, HttpServletRequest httpServletRequest) {
-        System.out.println("Problem" + e.getMessage());
+        ModelAndView modelAndView = new ModelAndView("500-page");
+        modelAndView.addObject("code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        modelAndView.addObject("message", e.getMessage());
         log.error("Message: {}, url: {}", e.getMessage(), httpServletRequest.getRequestURL());
-        Map<String, String> modelMessage = new HashMap<>();
-        modelMessage.put("error", e.getMessage());
-        ModelAndView modelAndView = new ModelAndView("500-page",modelMessage, HttpStatus.INTERNAL_SERVER_ERROR);
-
         return modelAndView;
     }
 }
