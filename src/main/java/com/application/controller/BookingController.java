70571efb107f424fe1545050279517ca9571cfd9
lilia <liliamokhnatska@gmail.com>
2023-01-11 01:04:24 +0200

    create of new booking and view

--- src/main/java/com/application/controller/BookingController.java
+++ src/main/java/com/application/controller/BookingController.java
@@ -1,51 +1,91 @@
 package com.application.controller;
 
 import com.application.dto.BookingCreateRequest;
-import com.application.dto.SearchAvailableRoomsRequest;
-import com.application.dto.mapper.RoomMapper;
+import com.application.dto.BookingResponse;
+import com.application.dto.UserCreateRequest;
+import com.application.dto.mapper.BookingMapper;
+import com.application.model.Booking;
+import com.application.model.Room;
+import com.application.model.User;
 import com.application.service.IBookingService;
+import com.application.service.IRoomService;
+import com.application.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /*import java.awt.*;*/
 
-import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDate;
+import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/bookings")
 public class BookingController {
 
 
-
     IBookingService bookingService;
+    IRoomService roomService;
+
+    IUserService userService;
+
 
     @Autowired
-    public BookingController(IBookingService bookingService) {
+    public BookingController(IBookingService bookingService, IRoomService roomService,
+                             IUserService userService) {
         this.bookingService = bookingService;
+        this.userService = userService;
+        this.roomService = roomService;
     }
+
 
     @PostMapping("/create")
-    public String create(@ModelAttribute("booking") BookingCreateRequest bookingCreateRequest, Model model){
+    public String create(@ModelAttribute("booking") BookingCreateRequest bookingCreateRequest,
+                         Principal principal, Model model, BindingResult result) {
         List<Long> roomIdList = bookingCreateRequest.getRoomIdList();
+
         LocalDateTime dateIn = bookingCreateRequest.getDateIn();
         LocalDateTime dateOut = bookingCreateRequest.getDateOut();
-        return null;
+
+        String email = principal.getName();
+        User user = userService.readByEmail(email);
+
+        List<Room> roomsAvailableList = new ArrayList<>();
+        for (Long roomId : roomIdList) {
+            roomsAvailableList.add(roomService.readById(roomId));
+        }
+
+        int dayIn = dateIn.getDayOfYear();
+        int dayOut = dateOut.getDayOfYear();
+        int numberOfBookingDay = Math.abs(dayIn - dayOut);
+
+        double valueOfAllAvailableRoomsPerNight = 0;
+
+        for (Room room : roomsAvailableList) {
+            valueOfAllAvailableRoomsPerNight += room.getPricePerRoom();
+        }
+
+        double totalValue = valueOfAllAvailableRoomsPerNight * numberOfBookingDay;
+
+        bookingService.saveBooking(new Booking(dateIn, dateOut, totalValue, user, roomsAvailableList));
+        BookingResponse bookingResponse = BookingMapper.mapToDto(new Booking(dateIn, dateOut, totalValue, user, roomsAvailableList));
+        model.addAttribute("bookingResponse", bookingResponse);
+        return "bookings-info";
+
     }
 
 
+    @GetMapping("/{booking_id}/delete")
+    // @PreAuthorize("hasAuthority('MANAGER')")
+    public String delete(@PathVariable(value = "booking_id") long bookingId) {
 
-
+        bookingService.delete(bookingId);
+        return "redirect:/rooms/all";
+    }
 
 
 }
